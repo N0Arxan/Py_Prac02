bcfde04316b1ee7f02032fe10283fad0ea627b55
Arshan Hassanzadeh <arshan.hassanzadeh@uvic.cat>
2024-10-15 19:19:38 +0200

    completed 13

--- 2.13.py
+++ 2.13.py
@@ -1,5 +1,12 @@
-num = 1
 sum = 1
-for i in range(1,101):
-    n = i 
-
+diff = 1
+i = 0
+while diff > 0.001:
+    i += 1
+    n = 2**i
+    n2 = 2**(i+1)
+    res = 1/n
+    res2 = 1/n2
+    diff = res-res2
+    sum = sum + res 
+print(sum)
