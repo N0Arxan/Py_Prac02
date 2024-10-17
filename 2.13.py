fa814185aebf8a6bdcd1b6c901d1aeb00495d718
arshan.hz@yahoo.com <arshan.hz@yahoo.com>
2024-10-18 00:29:57 +0200

    Batman002

--- 2.13.py
+++ 2.13.py
@@ -1,10 +1,15 @@
 sum = 1
 diff = 1
 counter = 0
+power_of_2 = 1
+
 while diff > 0.001:
     counter += 1
-    curr_term = 1/(2**counter)
-    next_term = 1/(2**(counter+1))
-    diff = next_term - curr_term
+    power_of_2 *= 2
+    curr_term = 1/ (power_of_2)
+    next_term = 1/ (power_of_2 * 2)
+    diff = curr_term - next_term
+    print(diff)
     sum += curr_term 
+
 print(sum)
